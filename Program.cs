2876bb9029e4f775de430e0e4fdcf84e7837849c
Jayden Ranger <10012125@BCS.NET.NZ>
2017-08-28 11:41:20 +1200

    leedle lee

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NumberOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var number = 0; 
+           var counter = 10;
+
+            for( var i = 0; i< counter; i++)
+            {
+            
+            Console.WriteLine("Give me a number");
+            var isNumber = int.TryParse(Console.ReadLine(), out int input);
+            number += input; 
+
+            }
+
+            Console.WriteLine($"Your total = {number}");
+            Console.WriteLine($" The Average = {(double)number / counter}");
+            
+              
+        
+        
+        }
+
+
+    }
+}
